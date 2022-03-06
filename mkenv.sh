1cedcc96b568a66903947239bcac4b1b9042105c
chadhyer <chadhyer92@gmail.com>
2022-03-05 20:32:31 -0500

    created some basic files

---
+++ mkenv.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# SELF_NAME
+SELF_INFO=./moodmuse.info
+SELF_NAME=$(sed -e '/SELF_NAME=/!d' -e 's/SELF_NAME=//g' $SELF_INFO)
+
+# If SELF_NAME is empty then error exit
+if [ "$SELF_NAME" == "" ]
+then
+	echo "$(date "+%Y-%m-%d %T") [ERROR] SELF_NAME is not defined in info file!"
+	exit 0
+fi 
+
+# Make list of directories
+dir_list=($(sed -e 's/SELF_.*=//g' -e '/^\//!d' -e "s/$SELF_NAME\/$SELF_NAME.*/$SELF_NAME\//g" $SELF_INFO))
+
+# Make directories in list
+for dir in ${dir_list[@]}
+do 	if [ ! -f $dir ]
+	then 	mkdir -p $dir
+		echo "$(date "+%Y-%m-%d %T") [INFO] $dir has been created"
+	fi
+done
