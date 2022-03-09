310c8fa51e37dc3740a8782feade89681dc520cb
chadhyer <chadhyer92@gmail.com>
2022-03-09 00:46:42 -0500

    - updated ignore for auth* - updated conf template - added auth to py

--- moodmuze.py
+++ moodmuze.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
+#### Imported Packages ####
+from os.path import exists #check if file exists
+import os #run os commands
+import configparser #config file
+import datetime #for datetime in log entries
+import sys #exit
+import getopt #parameters
+import json #handle json data
+import re #regular expression
+
 #### Global Variables
 ## Constants
 SELF_INFO = '/etc/moodmuze/moodmuze.info'
@@ -14,14 +24,6 @@ SELF_CONF = os.popen(cmd).read().strip()
 ## Integers
 ## Booleans
 
-#### Imported Packages ####
-import os #run os commands
-import configparser #config file
-import datetime #for datetime in log entries
-import sys #exit
-import getopt #parameters
-import json #handle json data
-
 #### Help_Text ####
 def Help_Text():
   print( SELF_NAME + ' V' + SELF_MAJOR + '.' + SELF_MINOR + ' Help Text placeholder')
@@ -32,13 +34,15 @@ def Load_Conf():
   global LOG_LEVEL
   global BRIDGE_IP
   global AUTH_FILE
-  conf_exists = os.exists(SELF_CONF)
+  conf_exists = exists(SELF_CONF)
   if conf_exists != True:
     dt = datetime.datetime.now()
-    sys.exit(dt.strftime("%Y-%m-%d %H:%M:%S") + '    [ERROR]    Unable to find config file: ' + SELF_CONF + '! Exiting!!!'
+    sys.exit(dt.strftime("%Y-%m-%d %H:%M:%S") + '    [ERROR]    Unable to find config file: ' + SELF_CONF + '! Exiting!!!')
+  config = configparser.ConfigParser()
+  config.read(SELF_CONF)
   # Load Configuration Options
   LOG_FILE = config.get('Log', 'LOG_FILE')
-  LOG_LEVEL = config.get('Log', 'LOG_LEVEL')
+  LOG_LEVEL = int(config.get('Log', 'LOG_LEVEL'))
   BRIDGE_IP = config.get('Hue', 'BRIDGE_IP')
   AUTH_FILE = config.get('Hue', 'AUTH_FILE')
 
@@ -57,12 +61,29 @@ def Log_Message(level, message):
     label = 'DEBUG'
   log = open(LOG_FILE, 'a')
   if LOG_LEVEL == 1:
-    print(dt.strftime("%Y-%m-%d %H:%M:%S") + '    [' + label + ']    ' + message, file = log)
-    print(dt.strftime("%Y-%m-%d %H:%M:%S") + '    [' + label + ']    ' + message)
+    print(dt.strftime("%Y-%m-%d %H:%M:%S") + '    [' + label + ']    ' + str(message), file = log)
+    print(dt.strftime("%Y-%m-%d %H:%M:%S") + '    [' + label + ']    ' + str(message))
   elif level >= LOG_LEVEL:
-    print(dt.strftime("%Y-%m-%d %H:%M:%S") + '    [' + label + ']    ' + message, file = log)
-    print(dt.strftime("%Y-%m-%d %H:%M:%S") + '    [' + label + ']    ' + message)
+    print(dt.strftime("%Y-%m-%d %H:%M:%S") + '    [' + label + ']    ' + str(message), file = log)
+    print(dt.strftime("%Y-%m-%d %H:%M:%S") + '    [' + label + ']    ' + str(message))
   log.close()
+
+#### json_extract ####
+# Recursively fetch values from nested JSON
+def json_extract(obj, key):
+  arr = []
+  # Recursively search for values of key in JSON tree
+  def extract(obj, arr, key):
+    if isinstance(obj, dict):
+      for k, v in obj.items():
+        if isinstance(v, (dict, list)):
+          extract(v, arr, key)
+        elif k == key:
+          arr.append(v)
+    elif isinstance(obj, list):
+      for item in obj:
+        extract(item, arr, key)
+    return arr
 
 #### Parameters ####
 def Parameters(argv):
@@ -81,6 +102,75 @@ def Parameters(argv):
       Log_Message (1, "Debug Logging enabled.")
 
 #### Auth ####
+def Auth():
+  # Check that auth file exists
+  auth_exists = exists(AUTH_FILE)
+  if auth_exists == True:
+    with open(AUTH_FILE, 'r') as file:
+      USERNAME = file.read().rstrip()
+  else:
+    Log_Message(4, 'Athentication file is missing!')
+    sys.exit('Auth Missing')
+  # Load Auth Token
+  global AUTH_TOKEN
+  cmd = "cat " + AUTH_FILE
+  AUTH_TOKEN = os.popen(cmd).read().strip()
+
+#### #### Class Bridge #### ####
+class Bridge:
+  def __init__(self):
+    self.token = BRIDGE_IP + '/api/' + AUTH_TOKEN
+    self.name = ''
+    self.zigbeechannel = ''
+    self.bridgeid = ''
+    self.mac = ''
+    self.dhcp = ''
+    self.ipaddress = ''
+    self.netmask = ''
+    self.gateway = ''
+    self.proxyaddress = ''
+    self.proxyport = ''
+    self.UTC = ''
+    self.localtime = ''
+    self.timezone = ''
+    self.modelid = ''
+    self.datastoreversion = ''
+    self.swversion = ''
+    self.apiversion = ''
+    self.swupdate = '' #dict
+    self.swupdate2 = ''
+    self.linkbutton = ''
+    self.portalservices = ''
+    self.portalconnection = ''
+    self.portalstate = '' #dict
+    self.internetservices = '' #dict
+    self.factorynew = ''
+    self.replacesbridgeid = ''
+    self.backup = '' #dict
+    self.starterkitid = ''
+    self.whitelist = '' #dict
+
+  # Validate response of API interaction
+  def check_response(self, data):
+    if bool(re.match('\[{"error":{', data)):
+      json_error = json.loads(data)
+      # Example error [{'error': {'type': 1, 'address': '/', 'description': 'unauthorized user'}}]
+      #er_type = json_extract(json_error, 'type')
+      #er_address = json_extract(json_error, 'address')
+      #er_description = json_extract(json_error, 'description')
+      #print(str(er_description + ' at ' + er_address + ' type: ' + er_type))
+      Log_Message(4, str(json_error))
+      sys.exit('Error Occurred')
+
+  # Pull bridge config
+  def pull_config(self):
+    cmd = "curl " + self.token
+    data = os.popen(cmd).read().strip()
+    self.check_response(data)
 
 #### ==== Main Sequence ==== ####
-
+Load_Conf()
+Parameters(sys.argv[1:])
+Auth()
+myBridge = Bridge()
+myBridge.pull_config()
