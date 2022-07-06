8e0e856861cddf71776b592d98adf216b66d1034
chadhyer <chadhyer92@gmail.com>
2022-07-06 00:15:12 -0400

    Created get room html file and view that links url

--- bridge_con/views.py
+++ bridge_con/views.py
@@ -3,38 +3,32 @@ from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-import requests 
 from moodmuze.settings.common import BRIDGE_IP, BRIDGE_KEY
+import requests 
+import json
 
 # Create your views here.
 def say_hello(request):
 	# do things
     return HttpResponse('Hello World')
 
-@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+@api_view(['GET', 'POST'])
 def room(request):
     """
     GET: Return list of all groups
     POST: Create new group
-    PUT: Update existing group
     """
 
+    context = {}
+        
     if request.method == 'GET':
-        print('GET')
+
         url=f'http://{BRIDGE_IP}/api/{BRIDGE_KEY}/groups/'
         response = requests.get(url=url)
-        data = str(response.content, 'utf-8')
-        print(data)
-        return HttpResponse(data)
+        context['group'] = json.loads(str(response.content, 'utf-8'))
+
+        return render(request, "get_room.html", context)
 
     if request.method == 'POST':
         print('POST')
         return HttpResponse('You post it')
-
-    if request.method == 'PUT':
-        print('PUT')
-        return HttpResponse('You put it')
-
-    if request.method == 'DELETE':
-        print('DELETE')
-        return HttpResponse('You delete it')
